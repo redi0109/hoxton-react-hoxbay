9e7d833ce7573f28fd3a57e55f615309f6535266
Demir Boraj <demirboraj97@gmail.com>
2022-08-05 17:20:39 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import Header from './components/Header'
+
+function App() {
+  return (
+    <>
+      <Header />
+    
+
+      <main>
+        {/* Create your routes here. Don't forget to install the router package! */}
+      </main>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.tsx
@@ -0,0 +1,35 @@
+import { getRandomColor } from "../helpers";
+
+function Header() {
+  const randomColor = getRandomColor();
+  return (
+    <header
+      className="header"
+      style={{
+        // @ts-ignore
+        ["--random-colour"]: `var(--${randomColor})`
+      }}
+    >
+      <div className="header__logo" style={{ color: randomColor }}>
+        Hoxbay
+      </div>
+      <nav className="header__nav">
+        <ul>
+          <li>
+            {/* Create here a link to this page */}
+            Home
+          </li>
+          <li>
+            {/* Create here a link to this page */}
+            Categories
+          </li>
+          <li>
+            {/* Create here a link to this page */}
+            Basket
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+export default Header;
